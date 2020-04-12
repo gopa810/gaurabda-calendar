5eeb48ccb2c603903d2ff17f8dfb9d8757dec59f
gopa810 <root@gopal.home.sk>
2020-04-12 10:12:15 +0200

    0.7.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 
-curr_ver = '0.7.5'
+curr_ver = '0.7.6'
 
 setup(
   name = 'gaurabda',         # How you named your package folder (MyLib)
