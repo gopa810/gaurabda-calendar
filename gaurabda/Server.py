725ae1f63266005c89b3ad8221dc149e39dc3b67
gopa810 <root@gopal.home.sk>
2020-06-17 14:44:15 +0200

    added server

---
+++ gaurabda/Server.py
@@ -0,0 +1,70 @@
+import flask
+from flask import request, jsonify
+
+import gaurabda.TCalendar as cal
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Gaurabda Calendar Web API</h1>
+<p>API for distant calculation of gaudiya vaisnava calendar.</p>'''
+
+
+@app.route('/api/v1/calculate/calendar', methods=['GET'])
+def api_all():
+    query_parameters = request.args
+
+    
+
+    return jsonify(all_books)
+
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_filter():
+    query_parameters = request.args
+
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
+    return jsonify(results)
+
+app.run()
