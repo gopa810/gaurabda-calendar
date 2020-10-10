62cedc1e940d04d6739b2d6cdc065b29c1a8b824
Nrsimha <nrsimha@getbhakti.com>
2020-10-10 19:06:41 +0200

    Fixed issue with not loading README.md and cleaned setup.py

--- setup.py
+++ setup.py
@@ -1,39 +1,31 @@
-from distutils.core import setup
+import setuptools
 
-# read the contents of your README file
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+current_version = "0.7.7"
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-curr_ver = '0.7.7'
-
-setup(
-  name = 'gaurabda',         # How you named your package folder (MyLib)
-  packages = ['gaurabda', 'gaurabda.GCEnums'],   # Chose the same as "name"
-  include_package_data = True,
-  package_data = {'gaurabda': ['res/*.*']},
-  version = curr_ver,      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Calculation of Gaurabda calendar (Gaudiya Vaishnava calendar)',   # Give a short description about your library
-  author = 'Gopal',                   # Type in your name
-  author_email = 'root@gopal.home.sk',      # Type in your E-Mail
-  url = 'https://github.com/gopa810/gaurabda-calendar',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gopa810/gaurabda-calendar/archive/' + curr_ver + '.tar.gz',    # I explain this later on
-  keywords = ['gaurabda', 'vaisnava', 'vaishnava', 'ISKCON', 'GCAL'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-      ],
-  classifiers=[
-    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
-      long_description='Gaudiya Vaisnava calendar calculation utilities.',
+setuptools.setup(
+    name='gaurabda',
+    version=current_version,
+    author="Gopal",
+    author_email="root@gopal.home.sk",
+    license="MIT",
+    description="Calculation of Gaurabda calendar (Gaudiya Vaishnava calendar)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gopa810/gaurabda-calendar",
+    download_url='https://github.com/gopa810/gaurabda-calendar/archive/' + current_version + '.tar.gz',
+    keywords=['gaurabda', 'vaisnava', 'vaishnava', 'ISKCON', 'GCAL'],
+    packages=setuptools.find_packages(),
+    package_data={'gaurabda': ['res/*.*']},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        'Topic :: Software Development :: Build Tools',
+    ],
+    python_requires='>=3.4',
 )
